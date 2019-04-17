191be30ed5180ea5c7949094c7a984b4c0dd437a
Artyom <vertigo117@yandex.ru>
2019-04-17 14:48:35 +0300

    Изменения дизайна истории
    Добавил пробелы между знаками (так лучше смотрится), увеличил шрифт и сделал цвет текста на 25% темнее.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -66,7 +66,7 @@ namespace Calculator
             lblShowUp.Content = "";
             foreach(string i in history)
             {
-                lblShowUp.Content += i;
+                lblShowUp.Content += i+" ";
             }
            
         }
